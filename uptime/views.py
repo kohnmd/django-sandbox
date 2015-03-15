b7b37c9305cb07d5d7eb666741ddf15fd0595185
Mike Kohn <kohnmd@gmail.com>
2015-03-15 00:18:31 -0600

    Began connecting to Uptime Robot API

--- uptime/views.py
+++ uptime/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import requests
+
+def index(request):
+    apiKey = 'm776710682-a0e34d5943f5727e485c1627'
+
+    request = {
+        'apiKey': apiKey,
+        'monitors': 776710682,
+        'logs': 1,
+        'format': 'json',
+        'noJsonCallback': 1
+    }
+    r = requests.get('https://api.uptimerobot.com/getMonitors', params=request)
+    data = r.json()
+
+    return HttpResponse(data.get('monitors')['monitor'][0]['status'])
